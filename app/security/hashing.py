c1e33e6e1c3ee825bbe680cd3ea2c9c017f39150
ibcore-dev <thyagomecanicoapcar@gmail.com>
2026-04-14 20:03:39 -0300

    Update hashing.py

--- app/security/hashing.py
+++ app/security/hashing.py
@@ -3,7 +3,9 @@ from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str):
+    password = password[:72]  # limite do bcrypt
     return pwd_context.hash(password)
 
 def verify_password(plain_password, hashed_password):
+    plain_password = plain_password[:72]  # evita erro 500
     return pwd_context.verify(plain_password, hashed_password)
